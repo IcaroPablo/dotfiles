4515346218aabfc6b84084a11075fb27f797cf7e
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-09-08 00:05:54 -0300

    improve neovim configs

--- private_dot_config/nvim/lua/executable_utils.lua
+++ private_dot_config/nvim/lua/executable_utils.lua
@@ -23,21 +23,19 @@ function utils.open_terminal_in_current_folder()
     -- -- os.execute('echo create ~/pou.sh ' .. cwd .. ' > ~/fifo.file &')
 
     local nvim_socket = "export NVIM_SOCKET=" .. vim.g.current_port
-    local terminal_command = nvim_socket  .. " ; " .. os.getenv('NVIM_TERMINAL') .. ' -e '
-    -- todo vim.fn.getcwd não traz a pasta onde o arquivo tá aberto
-    local full_command = 'cd ' .. vim.fn.getcwd() .. ' ; ' .. nvim_socket .. terminal_command .. 'ksh &'
+    local initial_folder = "export INITIAL_FOLDER=" .. vim.fn.expand('%:h')
+    local terminal_command = nvim_socket  .. " ; " .. initial_folder .. " ; " .. os.getenv('NVIM_TERMINAL')
+    local full_command = terminal_command .. ' 2>/dev/null &'
 
     os.execute(full_command)
-    -- os.execute("export NVIM_SOCKET=" .. vim.g.current_port .. " ; " .. os.getenv('NVIM_TERMINAL') .. ' -e lf &')
 end
 
 function utils.open_terminal_in_project_root()
     local nvim_socket = "export NVIM_SOCKET=" .. vim.g.current_port
-    local terminal_command = nvim_socket  .. " ; " .. os.getenv('NVIM_TERMINAL') .. ' -e '
-    -- todo vim.fn.getcwd não traz a pasta onde o arquivo tá aberto
-
     local project_folder = vim.fs.root(0, {".git", "mvnw", "gradlew", "pom.xml"})
-    local full_command = 'cd ' .. (project_folder ~= nil and project_folder or vim.fn.getcwd()) .. ' ; ' .. nvim_socket .. terminal_command .. 'ksh &'
+    local initial_folder = "export INITIAL_FOLDER=" .. (project_folder ~= nil and project_folder or vim.fn.expand('%:h'))
+    local terminal_command = nvim_socket  .. " ; " .. initial_folder .. " ; " .. os.getenv('NVIM_TERMINAL')
+    local full_command = terminal_command .. ' 2>/dev/null &'
 
     os.execute(full_command)
 end
