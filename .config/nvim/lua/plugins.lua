658edfa38c4b50c336e8b6b764716578e500b66e
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-09-15 13:12:02 -0300

    format file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,13 +8,13 @@
 
 -- bootstrap packer
 local ensure_packer = function()
-  local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+        vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 -- Autosource plugins file after saving
