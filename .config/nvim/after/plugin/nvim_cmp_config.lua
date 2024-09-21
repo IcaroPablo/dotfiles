a6a32d0524d2c472419c7d4dd2c8a231ad5f45fd
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-09-20 23:53:01 -0300

    improve mappings

--- .config/nvim/after/plugin/nvim_cmp_config.lua
+++ .config/nvim/after/plugin/nvim_cmp_config.lua
@@ -47,10 +47,15 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        -- ['<CR>'] = cmp.mapping.confirm({
-        --     behavior = cmp.ConfirmBehavior.Replace,
-        --     select = true
-        -- }),
+        ['<CR>'] = cmp.mapping(
+            function(fallback)
+                if cmp.visible() and cmp.get_active_entry() then
+                    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                else
+                    fallback()
+                end
+            end,
+            {'i', 's'}),
         ['<Tab>'] = cmp.mapping(
             function(fallback)
                 if cmp.visible() then
