1efa2da89a8a9748aa84cc45bf59e4d3580aac31
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-09-24 15:11:37 -0300

    getting rid of some plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -46,105 +46,75 @@ require('packer').init({
 })
 
 return require('packer').startup(function(use)
-    -- tabnine
-        -- https://github.com/tzachar/cmp-tabnine
-        -- https://www.tabnine.com/install/neovim
+    use 'wbthomason/packer.nvim'
     use({
         "lewis6991/gitsigns.nvim",
-        -- event = "BufRead",
+        event = "BufRead",
         requires = { "nvim-lua/plenary.nvim" },
-        -- config = require('gitsigns').setup
+        config = function()
+            require('gitsigns').setup({
+                on_attach = function(bufnr)
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                      if vim.wo.diff then
+                        vim.cmd.normal({']c', bang = true})
+                      else
+                        require('gitsigns').nav_hunk('next')
+                      end
+                    end)
+
+                    map('n', '[c', function()
+                      if vim.wo.diff then
+                        vim.cmd.normal({'[c', bang = true})
+                      else
+                        require('gitsigns').nav_hunk('prev')
+                      end
+                    end)
+
+                    -- Actions
+                    map('n', '<leader>hr', require('gitsigns').reset_hunk)
+                    map('n', '<leader>hR', require('gitsigns').reset_buffer)
+                    map('n', '<leader>hp', require('gitsigns').preview_hunk)
+                    map('n', '<leader>td', require('gitsigns').toggle_deleted)
+
+                    -- Text object
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end
+            })
+        end
     })
-    -- use 'nvim-telescope/telescope-file-browser.nvim' -- file browser for telescope
-    -- use 'jose-elias-alvarez/null-ls.nvim'
-    -- use 'windwp/nvim-autopairs'
-    -- use 'norcalli/nvim-colorizer.lua'
-    ---- COLORIZER: add a colored background for color codes
-    --use({
-        --"norcalli/nvim-colorizer.lua",
-        --ft = {
-            --"html",
-            --"css",
-            --"sass",
-            --"vim",
-            --"lua",
-            --"javascript",
-            --"typescript",
-            --"dosini",
-            --"ini",
-            --"conf",
-            --"json",
-            --"cfg",
-        --},
-        --cmd = { "ColorizerToggle" },
-        --config = function()
-            --require("colorizer").setup()
-            --vim.cmd("ColorizerAttachToBuffer")
-        --end,
-    --})
-    --use("nvim-lua/lsp-status.nvim")
-    --use({
-        --"folke/zen-mode.nvim",
-        --wants = "twilight.nvim",
-        --cmd = "ZenMode",
-        --config = function()
-            --require("configs.zen")
-        --end,
-    --})
-    --use({
-        --"folke/twilight.nvim",
-        --cmd = "Twilight",
-        --config = function()
-            --require("twilight").setup()
-        --end,
-    --})
+    use({
+        "folke/zen-mode.nvim",
+        requires = {
+            "folke/twilight.nvim",
+            cmd = "Twilight",
+            config = function()
+                require("twilight").setup()
+            end,
+        },
+        cmd = "ZenMode",
+    })
     use {
         'sindrets/diffview.nvim',
         requires = {
             "kyazdani42/nvim-web-devicons",
         }
-        --cmd = {
-            --"DiffviewOpen",
-            --"DiffviewFileHistory",
-        --},
-        --config = function()
-            --require("configs.diffview")
-        --end,
     }
-    use({ -- INDENT-BLANKLINE: display indent lines (even on blank lines)
-        "lukas-reineke/indent-blankline.nvim",
-        -- cmd = {
-        --     "IndentBlanklineEnable",
-        --     "IndentBlanklineToggle",
-        -- },
-    })
-    use({
-        'oysandvik94/curl.nvim',
-        requires = {
-            'mfussenegger/nvim-dap'
-        }
-    })
-    --use({ -- NEOSCROLL: smooth scrolling
-        --"karb94/neoscroll.nvim",
-        --event = "WinScrolled",
-        --config = function()
-            --require("configs.neoscroll")
-        --end,
-    --})
-    --use({
-        --"b3nj5m1n/kommentary",
-        --event = "CursorMoved",
-        --config = function()
-            --require("configs.comment")
-        --end,
-    --})
-    --use({
-        --"max397574/better-escape.nvim",
-        --event = "InsertEnter",
-        --config = function()
-            --require("configs.escape")
-        --end,
-    --})
+    use 'mbbill/undotree'
+    use 'lukas-reineke/indent-blankline.nvim'
+    -- use({
+    --     'oysandvik94/curl.nvim',
+    --     requires = {
+    --         'mfussenegger/nvim-dap'
+    --     }
+    -- })
+    -- use 'windwp/nvim-autopairs'
     --use({
         --"LunarWatcher/auto-pairs",
         --event = "BufEnter",
@@ -152,11 +122,6 @@ return require('packer').startup(function(use)
             --require("configs.autopairs")
         --end,
     --})
-    --use({
-        --"junegunn/vim-easy-align",
-        --keys = "<Plug>(EasyAlign)",
-    --})
-    use { "mbbill/undotree" }
     --use({
         --"ur4ltz/surround.nvim",
         --event = "BufEnter",
@@ -168,198 +133,36 @@ return require('packer').startup(function(use)
             --})
         --end,
     --})
-    --use({
-        --"tversteeg/registers.nvim",
-        --event = "BufEnter",
-        --config = function()
-            --vim.g.registers_show_empty_registers = 0
-            --vim.g.registers_hide_only_whitespace = 1
-            --vim.g.registers_window_border = "rounded"
-            --vim.g.registers_window_max_width = 50
-            --vim.g.registers_window_min_height = 3
-        --end,
-    --})
-    --use({
-        --"godlygeek/tabular",
-    --})
-    --use({
-        --"chentau/marks.nvim",
-        --ft = "markdown",
-        --config = function()
-            --require("configs.marks")
-        --end,
-    --})
-    --use({
-        --"nmac427/guess-indent.nvim",
-        --event = "BufEnter",
-        --config = function()
-            --require("guess-indent").setup()
-        --end,
-    --})
-    use 'shortcuts/no-neck-pain.nvim'
     use 'vinnymeller/swagger-preview.nvim'
-    use 'wbthomason/packer.nvim'
     use 'l3mon4d3/luasnip'
     use 'saadparwaiz1/cmp_luasnip'
-    use 'tpope/vim-commentary'
-    ---- TS-RAINBOW: treesitter rainbow parentheses
     --use({
-        --"p00f/nvim-ts-rainbow",
-        --event = "BufRead",
+        --"quangnguyen30192/cmp-nvim-ultisnips",
+        --after = "nvim-cmp",
     --})
-    ----
-    ---- TS-PLAYGROUND: see treesitter syntax tree
     --use({
-        --"nvim-treesitter/playground",
-        --cmd = { "TSPlaygroundToggle" },
+        --"sirver/ultisnips",
+        --event = "InsertEnter",
+        --setup = function()
+            --vim.g.UltiSnipsExpandTrigger = "<nop>"
+            --vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+            --vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
+            --vim.g.UltiSnipsRemoveSelectModeMappings = 0
+        --end,
+        --config = function()
+            --vim.cmd('let g:UltiSnipsSnippetDirectories=[$HOME."/.config/nvim/ultisnips"]')
+        --end,
     --})
+    use 'nvim-treesitter/nvim-treesitter-context'
     use({
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate"
-        -- config = function()
-        --     require("configs.treesitter")
-        -- end,
     })
-    use 'neovim/nvim-lspconfig'
-    --use({
-        --"neovim/nvim-lspconfig",
+    use({
+        "neovim/nvim-lspconfig",
         --event = "BufRead",
         --after = "cmp-nvim-lsp",
-        --config = function()
-            --require("configs.lspconfig")
-        --end,
-    --})
-    --use({
-        --"ray-x/lsp_signature.nvim",
-        --event = "InsertEnter",
-    --})
-    --use({
-        --"simrat39/symbols-outline.nvim",
-        --cmd = {
-            --"SymbolsOutline",
-            --"SymbolsOutlineOpen",
-            --"SymbolsOutlineClose",
-        --},
-        --setup = function()
-            --require("configs.outline")
-        --end,
-    --})
-    ----
-    ---- GLOW-HOVER: uses glow for lsp hover diagnostics
-    ----[[ use({
-        --"JASONews/glow-hover",
-        --config = function()
-            --require("glow-hover").setup({
-                --max_width = 50,
-                --padding = 10,
-                --border = "rounded",
-                --glow_path = "glow",
-            --})
-        --end,
-    --}) ]]
-    ----
-    ---- TEXLABCONFIG: texlab stuff
-    --use({
-        --"f3fora/nvim-texlabconfig",
-        --config = function()
-            --require("texlabconfig").setup()
-        --end,
-        --ft = { "tex", "bib" },
-        --cmd = { "TexlabInverseSearch" },
-    --})
-    ----
-    ---- TROUBLE: pretty list of diagnostics
-    --use({
-        --"folke/trouble.nvim",
-        --requires = "kyazdani42/nvim-web-devicons",
-        --config = function()
-            --require("configs.trouble")
-        --end,
-    --})
-    ----
-    ---- NULL-LS: LSP goodies
-    --use({
-        --"jose-elias-alvarez/null-ls.nvim",
-        --event = "BufRead",
-        --config = function()
-            --require("configs.null")
-        --end,
-    --})
-    ----
-    ---- LSP-LINES: replace virtual text with diagnostic lines
-    ----[[ use({
-        --"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        --event = "BufRead",
-        --config = function()
-            --require("lsp_lines").register_lsp_virtual_lines()
-        --end,
-    --}) ]]
-    ---- -_-_-_-_- DAP _-_-_-_-_-_
-    ---- DAP-PYTHON: Debugging python
-    --use({
-        --"mfussenegger/nvim-dap-python",
-        --after = "nvim-dap",
-        --config = function()
-            --require("dap-python").setup(
-                --"/home/lckdscl/.local/share/applications/debugpy/bin/python",
-                --{ justMyCode = false }
-            --)
-        --end,
-    --})
-    ----
-    ---- DAP: API for debuggers
-    --use({
-        --"mfussenegger/nvim-dap",
-        --module = "dap",
-        --config = function()
-            --require("configs.dap")
-        --end,
-    --})
-    ----
-    ---- DAP-VIRTUAL-TEXT: Display variable expressions as virtual text
-    --use({
-        --"theHamsta/nvim-dap-virtual-text",
-        --after = "nvim-dap",
-        --config = function()
-            --require("nvim-dap-virtual-text").setup()
-        --end,
-    --})
-    ----
-    ---- DAP-UI: Sane ui for debuggers
-    --use({
-        --"rcarriga/nvim-dap-ui",
-        --module = "dapui",
-        --requires = { "mfussenegger/nvim-dap" },
-    --})
-    ----
-    ---- HILINKTRACE: See current highlight group
-    ----[[ use({
-        --"gerw/vim-HiLinkTrace",
-    --}) ]]
-    use 'anuvyklack/pretty-fold.nvim'
-    --use({
-        --"lewis6991/foldsigns.nvim",
-        --event = "BufRead",
-        --config = function()
-            --require("foldsigns").setup({
-                --exclude = { "LspDiagnosticsSignWarning" },
-            --})
-        --end,
-    --})
-    ----[[ use({
-        --"lewis6991/cleanfold.nvim",
-        --commit = "ed54df0",
-        --event = "BufRead",
-        --config = function()
-            --require("cleanfold").setup()
-        --end,
-    --}) ]]
-    use {
-        'mfussenegger/nvim-jdtls',
-        requires = {
-            'mfussenegger/nvim-dap'
-        }
-    }
+    })
     use {
         'goolord/alpha-nvim',
         requires = {
@@ -420,16 +223,6 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
-    -- use {
-    --     "nvim-telescope/telescope-frecency.nvim",
-    --     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    -- }
-    use {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-        -- run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
-        run = "gmake",
-    }
     use {
         'nvim-telescope/telescope.nvim',
         -- tag = '0.1.8',
@@ -438,7 +231,7 @@ return require('packer').startup(function(use)
             -- consider installing ripgrep
             -- consider installing sharkdp/fd
             'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-ui-select.nvim'
+            'nvim-telescope/telescope-ui-select.nvim',
             -- {
             --     "nvim-telescope/telescope-file-browser.nvim",
             --     module = "telescope",
@@ -446,7 +239,17 @@ return require('packer').startup(function(use)
             -- {
             --     "nvim-telescope/telescope-fzf-native.nvim",
             --     cmd = "Telescope",
-            --     run = "make",
+            -- },
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                -- run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+                -- run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+                -- run = "make",
+                run = "gmake",
+            }
+            -- {
+            --     "nvim-telescope/telescope-frecency.nvim",
+            --     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
             -- },
             -- {
             --     "jvgrootveld/telescope-zoxide",
@@ -471,6 +274,7 @@ return require('packer').startup(function(use)
     }
     use {
         'hrsh7th/nvim-cmp',
+        --after = "ultisnips",
         requires = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-cmdline",
@@ -480,65 +284,35 @@ return require('packer').startup(function(use)
             'hrsh7th/vim-vsnip'
         },
     }
-    use({
-        "stevearc/oil.nvim",
-        config = function()
-            require("oil").setup()
-        end
-    })
     --use({
-        --"sirver/ultisnips",
-        --event = "InsertEnter",
-        --setup = function()
-            --vim.g.UltiSnipsExpandTrigger = "<nop>"
-            --vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
-            --vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
-            --vim.g.UltiSnipsRemoveSelectModeMappings = 0
-        --end,
+        --"mfussenegger/nvim-dap-python",
+        --after = "nvim-dap",
         --config = function()
-            --vim.cmd('let g:UltiSnipsSnippetDirectories=[$HOME."/.config/nvim/ultisnips"]')
-        --end,
-    --})
-    --use({
-        --"hrsh7th/nvim-cmp",
-        --after = "ultisnips",
-        --config = function()
-            --require("configs.cmp")
-        --end,
-    --})
-    --use({
-        --"quangnguyen30192/cmp-nvim-ultisnips",
-        --after = "nvim-cmp",
-    --})
-    --use({
-        --"hrsh7th/cmp-nvim-lsp",
-        --after = "nvim-cmp",
-    --})
-    --use({
-        --"hrsh7th/cmp-path",
-        --after = "cmp-nvim-lsp",
-    --})
-    --use({
-        --"hrsh7th/cmp-buffer",
-        --after = "cmp-path",
-    --})
-    --use({
-        --"gelguy/wilder.nvim",
-        --run = ":UpdateRemotePlugins",
-        --event = "CmdlineEnter",
-        --config = function()
-            --vim.cmd([[ source $HOME/.config/nvim/lua/configs/wilder.vim ]])
+            --require("dap-python").setup(
+                --"/home/lckdscl/.local/share/applications/debugpy/bin/python",
+                --{ justMyCode = false }
+            --)
         --end,
     --})
     use {
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = "v2.x",
+        'mfussenegger/nvim-jdtls',
+        --module = "dap",
         requires = {
-            "nvim-lua/plenary.nvim",
-            "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim"
-        },
+            'mfussenegger/nvim-dap'
+        }
     }
+    --use({
+        --"theHamsta/nvim-dap-virtual-text",
+        --after = "nvim-dap",
+        --config = function()
+            --require("nvim-dap-virtual-text").setup()
+        --end,
+    --})
+    --use({
+        --"rcarriga/nvim-dap-ui",
+        --module = "dapui",
+        --requires = { "mfussenegger/nvim-dap" },
+    --})
 
     if packer_bootstrap then
         require('packer').sync()
