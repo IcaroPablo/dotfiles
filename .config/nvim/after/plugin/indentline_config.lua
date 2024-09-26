1a338185f9e063974701ee817b46c17f61b9b478
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-09-25 23:57:18 -0300

    removing comments

--- .config/nvim/after/plugin/indentline_config.lua
+++ .config/nvim/after/plugin/indentline_config.lua
@@ -1,5 +1,4 @@
 require("ibl").setup({
-    -- ['indent.char'] = '▏',
     indent = {
         char = '▏'
     },
@@ -11,20 +10,3 @@ require("ibl").setup({
         buftypes = {'terminal'}
     }
 })
-
--- -- verificar instalação
--- require('indentmini').setup({
---     char = '▏', -- "'│' 
---     exclude = {'alpha', 'dashboard', 'help', 'neo-tree', 'lsp-installer', 'neo-tree-popup', 'mason', 'TelescopePrompt', 'man'}
--- })
-
-
--- -- :echo &ft
--- vim.cmd [[
---     let g:indentLine_char = '▏' "'│' 
---     let g:indentLine_showFirstIndentLevel = 1
---     let g:indentLine_first_char = '▏' "'│' 
---     "let g:indentLine_leadingSpaceEnabled = 1
---     "let g:indentLine_leadingSpaceChar = '·'
---     let g:indentLine_fileTypeExclude = ['alpha', 'dashboard', 'help', 'neo-tree', 'lsp-installer', 'neo-tree-popup', 'mason', 'TelescopePrompt', 'man']
--- ]]
