e5d08bcfab83886d23692d4e71c582915a7dd9ea
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2024-10-10 09:59:20 -0300

    increment gitignore

--- .config/nvim/after/plugin/hop_config.lua
+++ .config/nvim/after/plugin/hop_config.lua
@@ -12,6 +12,7 @@ vim.keymap.set('', 'F', function() hop.hint_char1({ direction = directions.BEFOR
 vim.keymap.set('', 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, {remap=true})
 vim.keymap.set('', 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, {remap=true})
 vim.keymap.set('', '<leader>l', function() hop.hint_vertical() end, {remap=true})
-vim.keymap.set('', '<leader>cc', ':HopCamelCaseCurrentLine<CR>', {remap=true})
+-- vim.keymap.set('', '<leader>c', ':HopCamelCaseCurrentLine<CR>', {remap=true})
 vim.keymap.set('', '<leader>w', function() hop.hint_words({current_line_only = true}) end, {remap=true})
 -- vim.keymap.set('', '<leader>W', function() hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = true}) end, {remap=true})
+-- hopnodes
