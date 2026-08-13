553795cd11b6260d850c28471817f67dc8b0aada
IcaroPablo <ic4ro.p4blo@gmail.com>
2026-08-12 22:52:01 -0300

    chore(nvim): remove yaml_revealer.lua (arquivo morto)
    Continha apenas uma função comentada, sem nenhuma linha ativa.

--- .config/nvim/after/plugin/yaml_revealer.lua
+++
@@ -1,5 +0,0 @@
--- local on_attach_config = function(client, bufnr)
---     client.server_capabilities.document_formatting = false
---     require('mappings').setup_nvim_lsp_on_attach(bufnr)
--- end
-
